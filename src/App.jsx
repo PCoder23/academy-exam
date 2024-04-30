5d81a7ccdefd52ac54f2aeaa7f9985dc4b36e20f
PCoder23 <pranavkjha111@gmail.com>
2024-04-30 20:39:01 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Exams from "./components/Exams"
+
+
+const App = () => {
+
+  return (
+    <>
+    <Exams />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Exams.jsx
@@ -0,0 +1,62 @@
+import {useEffect,useState} from 'react'
+
+const Exams = () => {
+
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if (e.ctrlKey && e.shiftKey && e.key === 'I') {
+              // Display a message or perform another action to indicate developer tools are disabled
+              e.preventDefault(); // Prevent browser default behavior
+              alert('Developer tools are disabled');
+              return false; // Prevent further propagation of the event
+            }
+          };
+      
+          document.addEventListener('keydown', handleKeyDown);
+
+        const noSelectElements =
+            document.querySelectorAll(".no-select");
+        noSelectElements.forEach((element) => {
+            element.style.webkitUserSelect = "none";
+            element.style.mozUserSelect = "none";
+            element.style.msUserSelect = "none";
+            element.style.userSelect = "none";
+        });
+        const handleVisibilityChange = () => {
+            console.log("Visibility changed to", document.visibilityState);
+            if (document.hidden) {
+              alert("You are not allowed to switch tabs during the exam");    
+            }
+        };
+    
+        document.addEventListener("visibilitychange", handleVisibilityChange);
+    
+        return () => {
+          document.removeEventListener("visibilitychange", handleVisibilityChange);
+          document.removeEventListener('keydown', handleKeyDown);
+        };
+      }, []);
+
+
+    const handleContextMenu = (e) => {
+        e.preventDefault()
+        alert('Right click not allowed')
+    }
+    const handleCopyPaste = (e) => {
+        e.preventDefault()
+        alert('Copy-Paste not allowed')
+    }
+
+  return (
+    <div 
+    onContextMenu={handleContextMenu} 
+    onCopy={handleCopyPaste}
+    onCut={handleCopyPaste} 
+    onSelect={handleCopyPaste}
+    className='w-screen h-screen bg-slate-600 no-select'>
+        Exams
+    </div>
+  )
+}
+
+export default Exams
